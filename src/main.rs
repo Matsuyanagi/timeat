21ebaeb1cb9abd2887de388d5a733513fd2dce40
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2024-01-28 02:19:42 +0900

    引数に渡された複数の unixitime を日付文字列として出力する
    -u オプションで utc 出力する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+// use chrono::prelude::*;
+use chrono::{DateTime, Local, Utc};
+
+#[derive(Parser)]
+#[command(name="timeat",author,version,about,long_about=None)]
+struct Cli {
+    // unixtime: Option<u64>,
+    unixtime: Vec<i64>,
+
+    #[arg(short = 'u', long, default_value = "false")]
+    utc: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    for ut in cli.unixtime {
+        let utc: DateTime<Utc> = DateTime::from_timestamp(ut, 0).unwrap();
+        if cli.utc {
+            println!("unixtime: {} , utc: {}", ut, utc);
+        } else {
+            let local: DateTime<Local> = utc.with_timezone(&Local);
+            println!("unixtime: {} , local: {}", ut, local);
+        }
+    }
+
+    ()
 }
